9837f423f4f8d7f725029635ba094288355e8c3f
Maha Sherif <mshis@MacBook-Air-3.local>
2025-06-28 15:29:14 +0200

    what you changed

---
+++ client.py
@@ -0,0 +1,32 @@
+import socket
+import threading
+
+HOST = '127.0.0.1'  # Same as server
+PORT = 12345
+
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect((HOST, PORT))
+
+def receive():
+    while True:
+        try:
+            message = client.recv(1024).decode('utf-8')
+            if message == 'NAME':
+                client.send(input("Enter your name: ").encode('utf-8'))
+            else:
+                print(message)
+        except:
+            print("An error occurred!")
+            client.close()
+            break
+
+def write():
+    while True:
+        message = input('')
+        client.send(message.encode('utf-8'))
+
+receive_thread = threading.Thread(target=receive)
+receive_thread.start()
+
+write_thread = threading.Thread(target=write)
+write_thread.start()
